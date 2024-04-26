bcf5db09e2228e31b739fb002d9acdaf02212617
pavanch1999 <55751806+pavanch1999@users.noreply.github.com>
2024-04-26 11:38:24 +0530

    product controller (#1)
    * mong
    
    * middleware error
    
    * video 26

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const router=express.Router();
+import { getProducts, getProductByID } from '../controllers/productController.js';
+router.route('/').get(getProducts);
+
+router.route('/:id').get(getProductByID);
+
+export default router;
